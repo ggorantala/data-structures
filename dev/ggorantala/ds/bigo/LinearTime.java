464a1c7257a2846c4f97319eabda67701d66aeb6
Gopi Gorantala <gopishiva001@outlook.com>
2023-06-02 19:50:14 +0530

    file added

---
+++ dev/ggorantala/ds/bigo/LinearTime.java
@@ -0,0 +1,11 @@
+package dev.ggorantala.ds.bigo;
+/*
+ * This code is part of the course "Java Data Structures and Algorithms" for https://ggorantala.dev.
+ *
+ * Copyright (C) 2023 by Gopi Gorantala.
+ *
+ * @author ggorantala
+ * Date: 02/06/23
+ */
+
+public class LinearTime {}
